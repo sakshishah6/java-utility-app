5eeb8351b2ae63313133c4fde4239a0481e68651
ShararN <45515460+ShararN@users.noreply.github.com>
2020-12-25 13:05:56 -0500

    Update GUI.java
    GUI

--- ScheduleBuilder/src/driver/GUI.java
+++ ScheduleBuilder/src/driver/GUI.java
@@ -19,14 +19,11 @@ public class GUI {
     private JLabel notificationLabel;
     private JFrame frame;
     private JPanel panel;
-    //private JButton button;
     
     public GUI() {
       frame = new JFrame(); //this is the window
       
-      //button = new JButton("Click Me!!");
-      
-      panel = new JPanel();
+      panel = new JPanel(new BorderLayout());
       
       calendarLabel = new JLabel();
       timerLabel = new JLabel();
@@ -37,27 +34,14 @@ public class GUI {
       timerLabel.setText("Timer");
       scheduleLabel.setText("Schedule");
       notificationLabel.setText("Notifications");
-      
-      calendarLabel.setAlignmentX(Component.LEFT_ALIGNMENT);
-      timerLabel.setAlignmentX(Component.LEFT_ALIGNMENT);
-      //calendarLabel.setLocation(110, 20);
-      //calendarLabel.setBounds(20, 50, 100, 50);
-      //timerLabel.setBounds(40, 100, 100, 50);
-      scheduleLabel.setBounds(100, 50, 100, 50);
-      notificationLabel.setBounds(100, 100, 100, 50);
-      
-      
+         
       panel.setSize(300, 300);
-      //panel.setBorder(BorderFactory.createEmptyBorder(300,  300,  300,  300));
-      //panel.setLayout(new GridLayout(0, 1));
-      //panel.add(button);
-      //panel.add(label);
-      panel.add(calendarLabel, BorderLayout.WEST);
-      panel.add(timerLabel, BorderLayout.WEST);
-      panel.add(scheduleLabel, BorderLayout.EAST);
-      panel.add(notificationLabel, BorderLayout.EAST);
+      panel.setLayout(new GridLayout(2, 2));
+      panel.add(calendarLabel);
+      panel.add(timerLabel);
+      panel.add(scheduleLabel);
+      panel.add(notificationLabel);
       
-      //button.addActionListener(this);
       
       frame.add(panel, BorderLayout.CENTER);
       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -72,11 +56,4 @@ public class GUI {
 		new GUI();
 	}
 
-	/*
-  @Override
-  public void actionPerformed(ActionEvent e) {
-    count ++;
-    //label.setText("Number of clicks: " + count);
-  }
-*/
 }
