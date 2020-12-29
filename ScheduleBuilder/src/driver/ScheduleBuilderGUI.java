7d6bdb5e6e441ea9c38bbe844c966933c3197f2e
Sakshi <45515113+sakshi0206@users.noreply.github.com>
2020-12-29 12:44:41 -0500

    Update ScheduleBuilderGUI.java
    removed calendar method related code

--- ScheduleBuilder/src/driver/ScheduleBuilderGUI.java
+++ ScheduleBuilder/src/driver/ScheduleBuilderGUI.java
@@ -104,39 +104,10 @@ public class ScheduleBuilderGUI {
 		timeLabel.setHorizontalAlignment(JLabel.LEFT);
 		timeLabel.setVerticalAlignment(JLabel.CENTER);
 		
-		Calendar cal = Calendar.getInstance();
-		
-		//CalendarFeature(cal);
-		
-		int month = cal.get(Calendar.MONTH);
-		int day = cal.get(Calendar.DAY_OF_MONTH);
-		int year = cal.get(Calendar.YEAR);
-		dateLabel.setText("Date: "+month+"/"+day+"/"+year);
-
-		int hour = cal.get(Calendar.HOUR);
-		int min = cal.get(Calendar.MINUTE);
-		int sec = cal.get(Calendar.SECOND);
-		int meridian = cal.get(Calendar.AM_PM);
-		
-		
-		String hh = null;
-		String mm = null;
-		String ampm = null;
-		
-		if (hour < 10) hh = "0"+String.valueOf(hour);
-		else hh = String.valueOf(hour);
-		
-		if (min < 10) mm = "0"+String.valueOf(min);
-		else mm = String.valueOf(min);
-
-		if (meridian == 0) ampm = "AM";
-		else ampm = "PM";
-		
-		timeLabel.setText("Time: "+hh+":"+mm+" "+ampm);
-		
-		//to change time dynamically, try running a recursive timer
-		//every time the timer is at 0 sec, start it again and update the timeLabel (i.e. line 174)
-		
+		CalendarFeature calendarRun = new CalendarFeature();
+		calendarRun.getDate(dateLabel);
+		calendarRun.getTime(timeLabel);
+		calendarRun.runProgram();
 	}
 	
 	
