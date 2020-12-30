66f8f6e452baa21b5c8abd4bf42fb125f503a40d
Sakshi <45515113+sakshi0206@users.noreply.github.com>
2020-12-30 11:51:21 -0500

    Update ScheduleBuilderGUI.java
    added icons to tabs

--- ScheduleBuilder/src/driver/ScheduleBuilderGUI.java
+++ ScheduleBuilder/src/driver/ScheduleBuilderGUI.java
@@ -5,10 +5,13 @@ import runner.ScheduleFeature;
 import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.GridLayout;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.awt.image.BufferedImage;
 import java.util.Calendar;
 
 import javax.swing.AbstractButton;
@@ -29,6 +32,8 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.ListSelectionModel;
 import javax.swing.JScrollBar;
+import java.awt.Color;
+import java.awt.Font;
 
 public class ScheduleBuilderGUI {
 
@@ -45,41 +50,78 @@ public class ScheduleBuilderGUI {
 
 		// Panel 0: Home Page
 		JPanel p0 = new JPanel();
+        ImageIcon tab0Icon, resizedTab0Icon;
+        tab0Icon = new ImageIcon("src/images/home icon.png","iconImg0");
+        resizedTab0Icon = resizeIcons(tab0Icon);
 		homepage(p0);
 		
-		// Panel 1: Calendar
+		// Panel 1: Calendar      
 		JPanel p1 = new JPanel();
+        ImageIcon tab1Icon, resizedTab1Icon;
+        tab1Icon = new ImageIcon("src/images/calendar icon.png","iconImg1");
+        resizedTab1Icon = resizeIcons(tab1Icon);
 		calendar(p1);
 		
 		// Panel 2: Schedule
 		JPanel p2 = new JPanel();
+        ImageIcon tab2Icon, resizedTab2Icon;
+        tab2Icon = new ImageIcon("src/images/schedule icon.png","iconImg2");
+        resizedTab2Icon = resizeIcons(tab2Icon);
 		schedule(p2);
 
 		// Panel 3: Timer & Stopwatch
 		JPanel p3 = new JPanel();
+        ImageIcon tab3Icon, resizedTab3Icon;
+        tab3Icon = new ImageIcon("src/images/timer icon.png","iconImg3");
+        resizedTab3Icon = resizeIcons(tab3Icon);
 		timer(p3);
 		
 		// Panel 4: Settings
 		JPanel p4 = new JPanel();
+        ImageIcon tab4Icon, resizedTab4Icon;
+        tab4Icon = new ImageIcon("src/images/settings icon.png","iconImg4");
+        resizedTab4Icon = resizeIcons(tab4Icon);
 		settings(p4);
 		
 		// Add all panels to the tabbedPane & to frame
 		tabbedPane.addTab("Home Page", p0);
+		tabbedPane.addTab("Home Page", resizedTab0Icon, p0);
+		tabbedPane.setBackgroundAt(0, Color.WHITE);
+		
 		tabbedPane.addTab("Calendar", p1);
+		tabbedPane.addTab("Calendar", resizedTab1Icon, p1);
+		tabbedPane.setBackgroundAt(1, Color.WHITE);
+		
 		tabbedPane.addTab("Schedule", p2);
+		tabbedPane.addTab("Schedule", resizedTab2Icon, p2);
+		tabbedPane.setBackgroundAt(2, Color.WHITE);
 		
 		tabbedPane.addTab("Timer & Stopwatch", p3);
+		tabbedPane.addTab("Timer & Stopwatch", resizedTab3Icon, p3);
+		tabbedPane.setBackgroundAt(3, Color.WHITE);
+		
 		tabbedPane.addTab("Settings", p4);
-
+		tabbedPane.addTab("Settings", resizedTab4Icon, p4);
+		tabbedPane.setBackgroundAt(4, Color.WHITE);
+		
 		frame2.getContentPane().add(tabbedPane);
 		frame2.setVisible(true);
+	}
+	
+	//Resize images
+	public static ImageIcon resizeIcons(ImageIcon icon) {
+        Image img = icon.getImage();
+        Image newImg = img.getScaledInstance(20, 20, Image.SCALE_SMOOTH);
+        ImageIcon newIcon = new ImageIcon(newImg);
+        return newIcon;
 	}
 	
 	
 	// Home Page
 	public static void homepage(JPanel p0) {
 		p0.setLayout(new GridLayout(2,1));
-		
+		p0.setBackground(Color.WHITE);
+		p0.setOpaque(true);
 		JLabel logo = new JLabel(new ImageIcon("src/images/logo.png"));
 		p0.add(logo);
 		logo.setHorizontalAlignment(JLabel.CENTER);
@@ -94,8 +136,10 @@ public class ScheduleBuilderGUI {
 	
 	// Calendar
 	public static void calendar(JPanel p1) {
+	
 		p1.setLayout(new GridLayout(3, 1));
-		
+		p1.setBackground(Color.WHITE);
+		p1.setOpaque(true);
 		JLabel dateLabel = new JLabel("Date: ");
 		p1.add(dateLabel);
 		dateLabel.setHorizontalAlignment(JLabel.LEFT);
@@ -166,6 +210,7 @@ public class ScheduleBuilderGUI {
        String[] columnTasks = {"Tasks"};
 
        JTable tableTimes = new JTable(dataTimes, columnTime);
+       tableTimes.setFont(new Font("Trebuchet MS", Font.PLAIN, 11));
        tableTimes.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
        tableTimes.setRowSelectionAllowed(false);
        tableTimes.setEnabled(false);
@@ -178,6 +223,8 @@ public class ScheduleBuilderGUI {
        tableInput.setRowSelectionAllowed(false);
        tableInput.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
        
+       p2.setBackground(Color.WHITE);
+       p2.setOpaque(true);
        p2.add(tableInput.getTableHeader(), BorderLayout.PAGE_START);
        p2.add(tableInput, BorderLayout.CENTER);
 	}
@@ -187,6 +234,8 @@ public class ScheduleBuilderGUI {
 	public static void timer(JPanel p3) {
 		p3.add(new JLabel("Timer & Stopwatch"));
 		p3.setLayout(new GridLayout(1, 1));
+		p3.setBackground(Color.WHITE);
+		p3.setOpaque(true);
 	}
 	
 	
@@ -194,6 +243,8 @@ public class ScheduleBuilderGUI {
 	public static void settings(JPanel p4) {
 		p4.add(new JLabel("Settings"));
 		p4.setLayout(new GridLayout(1, 1));
+		p4.setBackground(Color.WHITE);
+		p4.setOpaque(true);
 	}
 	
 	
