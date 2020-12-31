eab90c3580688a1516336e72416652a5aada1fa6
Sakshi <45515113+sakshi0206@users.noreply.github.com>
2020-12-31 11:47:58 -0500

    Update TimerFeature.java
    added code to TimerFeature

--- ScheduleBuilder/src/runner/TimerFeature.java
+++ ScheduleBuilder/src/runner/TimerFeature.java
@@ -1,9 +1,26 @@
 package runner;
 
+import javax.swing.JLabel;
+import javax.swing.JRadioButton;
+
 public class TimerFeature extends Programs {
 
-	  public void runProgram() {
+	public void selectOption(JRadioButton timerRadioBtn, JRadioButton stopwatchRadioBtn, JLabel timerLabel, JLabel stopwatchLabel) {
+		if (timerRadioBtn.isSelected()) runTimer(timerLabel);
+		else if (stopwatchRadioBtn.isSelected()) runStopwatch(stopwatchLabel);
+	}
+	
+	public void runTimer(JLabel timerLabel) {
+		timerLabel.setVisible(true);
+	}
+	
+	public void runStopwatch(JLabel stopwatchLabel) {
+		stopwatchLabel.setVisible(true);
+	}
+	
+	@Override
+	public void runProgram() {
 		  
-	  }
+	}
 	
 }
