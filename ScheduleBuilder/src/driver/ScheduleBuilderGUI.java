ec227ed10343e682904ae84aa615e3684985ad02
Sakshi <45515113+sakshi0206@users.noreply.github.com>
2021-01-04 17:38:59 -0500

    Update ScheduleBuilderGUI.java

--- ScheduleBuilder/src/driver/ScheduleBuilderGUI.java
+++ ScheduleBuilder/src/driver/ScheduleBuilderGUI.java
@@ -3,6 +3,7 @@ package driver;
 import runner.CalendarFeature;
 import runner.ScheduleFeature;
 import runner.TimerFeature;
+import runner.StopwatchFeature;
 
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
@@ -10,8 +11,6 @@ import java.awt.Image;
 import java.awt.TextField;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.FocusEvent;
-import java.awt.event.FocusListener;
 import javax.swing.ButtonGroup;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -42,37 +41,37 @@ public class ScheduleBuilderGUI {
 		// Panel 0: Home Page
 		JPanel p0 = new JPanel();
         ImageIcon tab0Icon, resizedTab0Icon;
-        tab0Icon = new ImageIcon("src/images/home icon.png","iconImg0");
+        tab0Icon = new ImageIcon("src/media/home icon.png","iconImg0");
         resizedTab0Icon = resizeIcons(tab0Icon);
 		homepage(p0);
 		
 		// Panel 1: Calendar      
 		JPanel p1 = new JPanel();
         ImageIcon tab1Icon, resizedTab1Icon;
-        tab1Icon = new ImageIcon("src/images/calendar icon.png","iconImg1");
+        tab1Icon = new ImageIcon("src/media/calendar icon.png","iconImg1");
         resizedTab1Icon = resizeIcons(tab1Icon);
 		calendar(p1);
 		
 		// Panel 2: Schedule
 		JPanel p2 = new JPanel();
         ImageIcon tab2Icon, resizedTab2Icon;
-        tab2Icon = new ImageIcon("src/images/schedule icon.png","iconImg2");
+        tab2Icon = new ImageIcon("src/media/schedule icon.png","iconImg2");
         resizedTab2Icon = resizeIcons(tab2Icon);
 		schedule(p2);
 
-		// Panel 3: Timer & Stopwatch
+		// Panel 3: Timer
 		JPanel p3 = new JPanel();
         ImageIcon tab3Icon, resizedTab3Icon;
-        tab3Icon = new ImageIcon("src/images/timer icon.png","iconImg3");
+        tab3Icon = new ImageIcon("src/media/timer icon.png","iconImg3");
         resizedTab3Icon = resizeIcons(tab3Icon);
 		timer(p3);
 		
-		// Panel 4: Settings
+		// Panel 4: Stopwatch
 		JPanel p4 = new JPanel();
         ImageIcon tab4Icon, resizedTab4Icon;
-        tab4Icon = new ImageIcon("src/images/settings icon.png","iconImg4");
+        tab4Icon = new ImageIcon("src/media/settings icon.png","iconImg4");
         resizedTab4Icon = resizeIcons(tab4Icon);
-		settings(p4);
+		stopwatch(p4);
 		
 		// Add all panels to the tabbedPane & to frame
 		tabbedPane.addTab("Home Page", p0);
@@ -88,12 +87,12 @@ public class ScheduleBuilderGUI {
 
 		tabbedPane.setBackgroundAt(2, Color.WHITE);
 		
-		tabbedPane.addTab("Timer & Stopwatch", p3);
-		tabbedPane.addTab("Timer & Stopwatch", resizedTab3Icon, p3);
+		tabbedPane.addTab("Timer", p3);
+		tabbedPane.addTab("Timer", resizedTab3Icon, p3);
 		tabbedPane.setBackgroundAt(3, Color.WHITE);
 		
-		tabbedPane.addTab("Settings", p4);
-		tabbedPane.addTab("Settings", resizedTab4Icon, p4);
+		tabbedPane.addTab("Stopwatch", p4);
+		tabbedPane.addTab("Stopwatch", resizedTab4Icon, p4);
 		tabbedPane.setBackgroundAt(4, Color.WHITE);
 		
 		frame.getContentPane().add(tabbedPane);
@@ -114,7 +113,7 @@ public class ScheduleBuilderGUI {
 		p0.setLayout(new GridLayout(2,1));
 		p0.setBackground(Color.WHITE);
 		p0.setOpaque(true);
-		JLabel logo = new JLabel(new ImageIcon("src/images/logo.png"));
+		JLabel logo = new JLabel(new ImageIcon("src/media/logo.png"));
 		p0.add(logo);
 		logo.setHorizontalAlignment(JLabel.CENTER);
 		logo.setVerticalAlignment(JLabel.BOTTOM);
@@ -358,9 +357,9 @@ public class ScheduleBuilderGUI {
 	    
 	}
 	
-	// Settings
-	public static void settings(JPanel p4) {
-		p4.add(new JLabel("Settings"));
+	// Stopwatch
+	public static void stopwatch(JPanel p4) {
+		p4.add(new JLabel("Stopwatch"));
 		p4.setLayout(new GridLayout(1, 1));
 		p4.setBackground(Color.WHITE);
 		p4.setOpaque(true);
